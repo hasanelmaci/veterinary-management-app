f6815b6cfb134626286ce9673de124db1f79f5c2
= <mhasanelmaci@gmail.com>
2021-05-11 02:53:35 +0300

    index prod

--- index.js
+++ index.js
@@ -42,10 +42,12 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static("client/build"));
-app.get("*", (req,res)=>{
-  res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-})
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static("client/build"));
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+  })
+}
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port} !`);
